1a9e69700a5e531a520269b4653fccf90661a265
Admin <sergpas@kil.com>
2021-02-16 11:26:41 +0200

    Work

---
+++ src/com/alfa/work65/Main.java
@@ -0,0 +1,29 @@
+package com.alfa.work65;
+
+public class Main {
+    public static void main(String[] args) {
+        printPyramid(9);
+    }
+    public static void printPyramid (int height){
+        int curHeight = 0;
+        int maxHeight = height;
+        while(++curHeight <= height+1){
+            String reverseString="";
+            String outString="";
+            System.out.println();
+            int curRow=0;
+            while (++curRow <= maxHeight){      //Рисуем отступы в начале каждой строки
+                System.out.print(' ');
+            }
+            maxHeight--;
+            curRow=0;
+            while(++curRow < curHeight){
+                outString = outString+ curRow;
+                if(curRow < curHeight - 1){
+                    reverseString = curRow + reverseString;
+                }
+            }
+            System.out.print(outString + reverseString);
+        }
+    }
+}
