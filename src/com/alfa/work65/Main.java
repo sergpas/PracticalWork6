dbfada76da4a7d2d525ed631f298d3a8b165fc0b
Admin <sergpas@kil.com>
2021-02-17 19:32:03 +0200

    Work

--- src/com/alfa/work65/Main.java
+++ src/com/alfa/work65/Main.java
@@ -2,29 +2,33 @@ package com.alfa.work65;
 
 public class Main {
     public static void main(String[] args) {
-        printPyramid(9);
+        printPyramid(5);
     }
 
-    public static void printPyramid (int height){
+    public static void printPyramid(int height) {
+        if (height < 1 || height > 9) {
+            System.out.println("Incorrect parameter:" + height);
+            return;
+        }
         int curHeight = 0;
         int maxHeight = height;
-        while(++curHeight <= height+1){
-            String reverseString="";
-            String outString="";
-            System.out.println();
-            int curRow=0;
-            while (++curRow <= maxHeight){      //Рисуем отступы в начале каждой строки
+        while (++curHeight <= height + 1) {
+            int curNumber = 0;
+            while (++curNumber <= maxHeight) {      //Рисуем отступы в начале каждой строки
                 System.out.print(' ');
             }
-            maxHeight--;
-            curRow=0;
-            while(++curRow < curHeight){
-                outString = outString+ curRow;
-                if(curRow < curHeight - 1){
-                    reverseString = curRow + reverseString;
+            curNumber = 0;
+            String outString = "";
+            String reverseString = "";
+            while (++curNumber < curHeight) {
+                outString = outString + curNumber;
+                if (curNumber < curHeight - 1) {
+                    reverseString = curNumber + reverseString;
                 }
             }
             System.out.print(outString + reverseString);
+            System.out.println();
+            maxHeight--;
         }
     }
 }
