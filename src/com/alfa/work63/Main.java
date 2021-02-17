7322dc91c5d74abcf140b975ae26e3dbb257055a
Admin <sergpas@kil.com>
2021-02-17 18:22:16 +0200

    Work

--- src/com/alfa/work63/Main.java
+++ src/com/alfa/work63/Main.java
@@ -12,6 +12,8 @@ public class Main {
         char chr = getFirstCharacterOfWord(inString,numberWorld);
         if (chr != ' ') {
             System.out.println("First Symbol: "+chr);
+        }else{
+            System.out.println("Incorrect Number of World: "+numberWorld);
         }
     }
     public static char getFirstCharacterOfWord(String inString,int numberWorld){
@@ -20,15 +22,12 @@ public class Main {
         int curNumbWorld = 0;
         while ((idxNextWorld=inString.indexOf(' ',idxCurWorld)) > 0){
             if(++curNumbWorld == numberWorld) {
-                System.out.println("First Symbol: "+inString.substring(idxCurWorld,idxNextWorld).charAt(0));
-                return ' ';
+                return inString.substring(idxCurWorld,idxNextWorld).charAt(0);
             }
             idxCurWorld=idxNextWorld+1;
         }
-        if(++curNumbWorld == numberWorld) {
+        if(++curNumbWorld == numberWorld) { //Последнее слово
             return inString.substring(idxCurWorld).charAt(0);
-        }else{
-            System.out.println("Incorrect Number of World: "+numberWorld);
         }
         return ' ';
     }
